15d593105b9feb61d75016198eff9f290b9495e9
TianYY <62873029+mhw-Parker@users.noreply.github.com>
2021-02-07 00:26:21 +0800

    Add files via upload

--- src/readExcelData.cpp
+++ src/readExcelData.cpp
@@ -13,7 +13,7 @@ ExcelRead::ExcelRead()
     usedrange = NULL;
 }
 
-bool ExcelRead::readExcelData(QString& filename)
+bool ExcelRead::datarange_init(QString &filename, int& totalRow, int& totalCol)
 {
     excel = new QAxObject("Excel.Application");									//创建Excel对象连接驱动
     excel->dynamicCall("SetVisible(bool)",false);								//ture的打开Excel表 false不打开Excel表
@@ -25,19 +25,31 @@ bool ExcelRead::readExcelData(QString& filename)
     worksheet = worksheets->querySubObject("Item(int)",1);          			//获取第1个sheet表
     usedrange =worksheet->querySubObject("Usedrange");							//获取权限
 
-    int iRow = usedrange->property("Row").toInt();             					//数据起始行数和列数(可以解决不规则Excel)
-    int iCol = usedrange->property("Column").toInt();
+    iRow = usedrange->property("Row").toInt();             					//数据起始行数和列数(可以解决不规则Excel)
+    iCol = usedrange->property("Column").toInt();
 
-    int totalRow = usedrange->querySubObject("Rows")->property("Count").toInt();  //获取数据总行数
-    int totalCol = usedrange->querySubObject("Columns")->property("Count").toInt();
+    totalRow = usedrange->querySubObject("Rows")->property("Count").toInt();  //获取数据总行数
+    totalCol = usedrange->querySubObject("Columns")->property("Count").toInt();
     //
+    return true;
+}
+
+bool ExcelRead::readExcelData(QString& filename, vector<vector<float> >Data)
+{
+    int row = 0;
+    int col = 0;
+    if(datarange_init(filename,row,col))
+        cout<<"datarange_init successed !"<<endl;
+    else
+        return false;
     double time_start = (double)clock();
     // 逐行读取主表
-    for (int i = iRow+1; i <= totalRow; i++)
+    for (int i = iRow+1; i <= row; i++)
     {
-        for(int j = iCol; j <= totalCol; j++)
+        for(int j = iCol; j <= col; j++)
         {
             QString data = worksheet->querySubObject("Cells(int,int)",i,j)->dynamicCall(("Value2()")).value<QString>();
+            Data[i][j] = data.toFloat();
             string strdata = data.toStdString();
             printf("%s\t",strdata.c_str());
             //qDebug()<< data;
